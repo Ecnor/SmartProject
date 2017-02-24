97b67c3b8e7e46def5b3b9f176ee9a1d1d0d029c
Emine <bernardoneemine@gmail.com>
2017-02-24 16:57:26 +0100

    Added drawing background and output field.

--- src/main/MainWindowApplication.java
+++ src/main/MainWindowApplication.java
@@ -3,10 +3,16 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class MainWindowApplication {
 
 	private JFrame frame;
+	private JTextField tf_output;
 
 	/**
 	 * Launch the application.
@@ -36,8 +42,31 @@ public class MainWindowApplication {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setSize(800, 500);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel jp_drawing_bg = new JPanel();
+		jp_drawing_bg.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		jp_drawing_bg.setBackground(Color.WHITE);
+		jp_drawing_bg.setBounds(30, 50, 350, 350);
+		frame.getContentPane().add(jp_drawing_bg);
+		
+		JPanel jp_output = new JPanel();
+		jp_output.setBorder(new LineBorder(new Color(0, 0, 0)));
+		jp_output.setBounds(450, 200, 175, 80);
+		frame.getContentPane().add(jp_output);
+		jp_output.setLayout(null);
+		
+		tf_output = new JTextField();
+		tf_output.setBounds(10, 30, 150, 20);
+		jp_output.add(tf_output);
+		tf_output.setText("TEST");
+		tf_output.setColumns(10);
+		
+		JLabel lbl_output = new JLabel("Output :");
+		lbl_output.setBounds(10, 10, 50, 15);
+		jp_output.add(lbl_output);
 	}
-
 }
